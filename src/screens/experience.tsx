b23f8c9aa8e48534742e550972d147692c76cb9a
Jan Ancuta <jas.ancuta@gmail.com>
2024-06-23 00:42:15 +0200

    układ strony "experience"  wstępnie gotowy (pc)

--- src/screens/experience.tsx
+++ src/screens/experience.tsx
@@ -6,6 +6,20 @@ import useWindowSize from "hooks/windowSize";
 import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
+const SectionImage = ({src, translateY} : {src: string, translateY: number}) => {
+    return (
+        <div className="animated-image" style={{
+            transform: `translateY(${translateY}px)`,
+        }}>
+            <img
+                className="image-me image-hover"
+                src={src}
+            />
+        </div>
+    )
+};
+
 const Experience = () => {
     const navigate = useNavigate();
 
@@ -17,58 +31,57 @@ const Experience = () => {
     const sectionOneRef = useRef<HTMLDivElement>(null);
     const sectionTwoRef = useRef<HTMLDivElement>(null);
     const sectionThreeRef = useRef<HTMLDivElement>(null);
-
+    const [startPosition, setStartPosition] = useState(0);
+    const [imageOneY, setImageOneY] = useState(0);
+    const [imageTwoY, setImageTwoY] = useState(0);
+    const [imageThreeY, setImageThreeY] = useState(0);
 
     const handleTextScroll = (e: any)=> {
         const scrollPosition = textPartRef.current?.scrollTop || 0;
         //cannot use scrollY, don't know why
 
         const arr = [sectionOneRef, sectionTwoRef, sectionThreeRef];
-        console.log('scroll: ');
-        console.log(scrollPosition);
-
-        // console.log('first\' top: ');
-        // console.log(arr[0].current!.getBoundingClientRect().top);
 
         let minDist = Infinity;
         let minIndex = 0;
         for(let i=0; i<=2; i++){
-            console.log(i, ' -> ', arr[i].current!.getBoundingClientRect().top - scrollPosition);
-            if(Math.abs(arr[i].current!.getBoundingClientRect().top - scrollPosition) <= minDist){
-                minDist = Math.abs(arr[i].current!.getBoundingClientRect().top - scrollPosition);
+            console.log(i, ' -> ', arr[i].current!.getBoundingClientRect().top);
+            if(Math.abs(arr[i].current!.getBoundingClientRect().top - startPosition - 150) <= minDist){
+                minDist = Math.abs(arr[i].current!.getBoundingClientRect().top - startPosition - 150);
                 minIndex = i;
             }
         };
-        // alert(minIndex);
-        arr[minIndex].current!.scrollIntoView();
+
+        requestAnimationFrame(() =>
+            arr[minIndex].current!.scrollIntoView()
+        );
     };
 
     useEffect(() => {
-        console.log(scrollY);
+        setImageOneY(
+            (sectionOneRef.current!.getBoundingClientRect().top - startPosition) * 3
+        );
 
-        // const { top, bottom } = sectionOneRef.current!.getBoundingClientRect();
-        // console.log(top);
-        // console.log(bottom);
+        setImageTwoY(
+            (sectionTwoRef.current!.getBoundingClientRect().top - startPosition) * 3
+        );
 
-        // console.log('----------');
+        setImageThreeY(
+            (sectionThreeRef.current!.getBoundingClientRect().top - startPosition) * 3
+        );
 
-    }, [scrollY]);
+    }, [scrollY, startPosition]);
 
     useEffect(() => {
         setTextScrollHeight(textPartRef.current?.scrollHeight);
-
-        // if(sectionOneRef && sectionOneRef.current){
-        //     sectionOneRef.current!.getBoundingClientRect();
-        //     const array: number[] = [];
-        //     sections.map((section) => {
-        //         // array.push(section.current!.getBoundingClientRect().top)
-        //     });
-        //     setSectionsY(array);
-        // }
-    
     }, [width, height]);
 
     useEffect(() => {
+        if(sectionOneRef && sectionOneRef.current){
+            setStartPosition(sectionOneRef.current.getBoundingClientRect().top);
+            // alert(sectionOneRef.current.getBoundingClientRect().top);
+        }
+
         textPartRef.current?.addEventListener('scrollend', handleTextScroll);
         return () => {
             textPartRef.current?.removeEventListener('scrollend', handleTextScroll);
@@ -78,18 +91,22 @@ const Experience = () => {
     return (
         <main className="w-screen h-scalable bg-white relative flex overflow-hidden">
             <AnimatedBox className="h-screen w-screen p-20 mobile-flex split-2">
-                <div className="half-container">
-                    <img
-                        className="image-me image-hover"
-                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg"
-                    />
+                <div className="half-container h-full overflow-y-hidden">
+                    <SectionImage translateY={imageOneY} 
+                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg"/>
+
+                    <SectionImage translateY={imageTwoY} 
+                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg"/>
+
+
+                    <SectionImage translateY={imageThreeY} 
+                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo_5.jpg"/>
                 </div>
                 <div
                     className="half-container text-part overflow-y-hidden"
                 >
                     <div className="block label">
                         <ExperienceSVG />
-                        {scrollY}
                     </div>
                     <div className="achieved-text-container h-full">
                         <div className="achieved-text font-1 overflow-y-scroll" ref={textPartRef}>
@@ -105,6 +122,18 @@ const Experience = () => {
                                     expedita ducimus autem cupiditate alias
                                     adipisci sapiente, asperiores nemo magni,
                                     molestiae sit similique! Impedit sunt ipsam
+                                    quibusdam porro animi unde, quisquam vel
+                                    autem non amet consequatur! Unde adipisci
+                                    consequatur vel, eius aliquid dignissimos
+                                    hic veniam fuga. Debitis ab sapiente harum
+                                    rerum molestiae distinctio ea odit officiis
+                                    maxime vitae consequatur, ducimus voluptates
+                                    asperiores temporibus, provident, velit
+                                    adipisci. Placeat totam iure facere
+                                    corrupti, ratione accusantium nulla
+                                    exercitationem tenetur dicta impedit
+                                    cupiditate, harum fugit id, inventore quo ad
+                                    sit? Iste, unde.
                                     quibusdam porro animi unde, quisquam vel
                                     autem non amet consequatur! Unde adipisci
                                     consequatur vel, eius aliquid dignissimos
