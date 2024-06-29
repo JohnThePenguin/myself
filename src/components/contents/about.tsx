7588b971d3c5309527ffb8fae4f0dd82463f287b
Jan Ancuta <jas.ancuta@gmail.com>
2024-06-29 09:47:11 +0200

    tekst na strone about me #1

---
+++ src/components/contents/about.tsx
@@ -0,0 +1,17 @@
+import 'styles/content.css';
+
+const AboutMeContent = () => {
+    return (
+    <div className="text-content about-me">
+        <p>Hi, i am Janek.</p>
+        <p>I am 18 years old and month ago i have written polish matura exam, finishing high school.</p>
+        <p>When I was younger, my interests was about math, sport and art.</p>
+        <p>I was taking part in mathematic competitions, attending to drawing lessons, playing football and spending time on skatepark.</p>
+        <p>Since last classes of primary school, i have mainly focused on informatics. I love building my own projects,
+        and working on them with my friends. </p>
+        <p>I have also a lot of experience with algorithmic and competitive programming.</p>
+    </div>
+    );
+};
+
+export default AboutMeContent;
