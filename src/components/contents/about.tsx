abbc6932479accc305a1996d80ef1ab5d71ef729
Jan Ancuta <jas.ancuta@gmail.com>
2024-06-29 10:07:27 +0200

    about me - tekst

--- src/components/contents/about.tsx
+++ src/components/contents/about.tsx
@@ -1,16 +1,88 @@
-import 'styles/content.css';
+import { useInView } from "framer-motion";
+import { ReactNode, useRef } from "react";
+import "styles/content.css";
+
+const AText = ({ children }: { children: ReactNode }) => {
+    const ref = useRef<HTMLParagraphElement>(null);
+    const isInView = useInView(ref, { once: true });
+
+    return (
+        <p
+            ref={ref}
+            className="styled-text"
+            style={{
+                transform: isInView ? "none" : "translateX(-200px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
+        >
+            {children}
+        </p>
+    );
+};
 
 const AboutMeContent = () => {
     return (
-    <div className="text-content about-me">
-        <p>Hi, i am Janek.</p>
-        <p>I am 18 years old and month ago i have written polish matura exam, finishing high school.</p>
-        <p>When I was younger, my interests was about math, sport and art.</p>
-        <p>I was taking part in mathematic competitions, attending to drawing lessons, playing football and spending time on skatepark.</p>
-        <p>Since last classes of primary school, i have mainly focused on informatics. I love building my own projects,
-        and working on them with my friends. </p>
-        <p>I have also a lot of experience with algorithmic and competitive programming.</p>
-    </div>
+        <div className="text-content about-me">
+            <AText>Hi, i am Janek.</AText>
+            <AText>
+                I am 18 years old and month ago i have written polish matura
+                exam, finishing high school.
+            </AText>
+            <AText>
+                When I was younger, my interests was about math, sport and art.
+            </AText>
+            <AText>
+                I was taking part in mathematic competitions, attending to
+                drawing lessons, playing football and spending time on
+                skatepark.
+            </AText>
+            <AText>
+                Since last classes of primary school, i have mainly focused on
+                informatics. I love building my own projects, and working on
+                them with my friends.{" "}
+            </AText>
+            <AText>
+                I have also a lot of experience with algorithmic and competitive
+                programming.
+            </AText>
+        </div>
+    );
+};
+
+export const AboutMeContentMobileOne = () => {
+    return (
+        <div className="text-content about-me">
+            <AText>Hi, i am Janek.</AText>
+            <AText>
+                I am 18 years old and month ago i have written polish matura
+                exam, finishing high school.
+            </AText>
+            <AText>
+                When I was younger, my interests was about math, sport and art.
+            </AText>
+        </div>
+    );
+};
+
+export const AboutMeContentMobileTwo = () => {
+    return (
+        <div className="text-content about-me">
+            <AText>
+                I was taking part in mathematic competitions, attending to
+                drawing lessons, playing football and spending time on
+                skatepark.
+            </AText>
+            <AText>
+                Since last classes of primary school, i have mainly focused on
+                informatics. I love building my own projects, and working on
+                them with my friends.{" "}
+            </AText>
+            <AText>
+                I have also a lot of experience with algorithmic and competitive
+                programming.
+            </AText>
+        </div>
     );
 };
 
--- src/screens/mobile/about.tsx
+++ src/screens/mobile/about.tsx
@@ -15,6 +15,7 @@ import AncutaSVG from 'components/handwrites/ancuta';
 import YearsSVG from 'components/handwrites/years';
 import FullNameSVG from 'components/handwrites/fullName';
 import AnimatedBox from 'components/animations/animatedBox';
+import AboutMeContent, { AboutMeContentMobileOne, AboutMeContentMobileTwo } from 'components/contents/about';
 
 export default function AboutMobile() {
   const [progress, setProgress] = useState(0);
@@ -49,7 +50,11 @@ export default function AboutMobile() {
               </AnimatedBox>
             </SwiperSlide>
             <SwiperSlide style={{height: 'auto', overflowY: 'scroll', paddingTop: "3em"}} >
-              <TextContent/>
+              <AboutMeContentMobileOne/>
+            </SwiperSlide>
+
+            <SwiperSlide style={{height: 'auto', overflowY: 'scroll', paddingTop: "3em"}} >
+              <AboutMeContentMobileTwo/>
             </SwiperSlide>
           </Swiper>
         </SwiperSlide>
@@ -69,7 +74,7 @@ function TextContent(){
     <AnimatedBox style={{height: '100%', paddingBottom: '30px'}}>
 
   <span style={{paddingTop: '30px', textAlign: 'center', justifyContent: 'center', paddingBottom: '30px'}}>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed illum esse aperiam dolorem consectetur mollitia commodi reprehenderit vel ipsam, impedit qui exercitationem, molestias omnis ipsum repellat quo dolores, praesentium tempora molestiae vero. Voluptatem sed commodi nisi minus explicabo modi? Voluptates quia fugit libero voluptatem dignissimos in dolorum at eligendi accusantium ad harum eos sit reprehenderit, eum, error vero voluptate iure dicta? Consectetur nemo illum molestiae, natus nesciunt eius eveniet minus accusamus quia rerum repellat. Sunt quo maiores similique cumque rerum, a deserunt necessitatibus eveniet aut voluptatem. Voluptatibus, nam amet perferendis officia quo repudiandae asperiores eum vel harum eius in assumenda iste aperiam adipisci accusantium placeat tenetur. Ad id vero voluptatum recusandae, magni totam distinctio laboriosam excepturi mollitia modi nesciunt! Nihil optio commodi modi exercitationem, minima facilis maiores cum, eius vitae veniam rerum nam provident aliquam dolores nemo libero aut, reiciendis error cumque ullam nisi? Illum quo suscipit atque quis asperiores aperiam, facere expedita saepe amet, sunt, quasi quaerat possimus! Atque deleniti architecto aut quasi nihil, laborum quod rem explicabo iusto error similique quia culpa earum quae fuga commodi blanditiis voluptatum numquam autem dolores nemo quo ducimus fugiat. Similique perferendis id, minima quas laudantium ut? Praesentium ullam excepturi quod numquam illum?
+    <AboutMeContent/>
   </span>
     </AnimatedBox>
   );
