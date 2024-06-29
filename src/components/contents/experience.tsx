07545796ffeac5f04a742573d0e6fbfbca531d6a
Jan Ancuta <jas.ancuta@gmail.com>
2024-06-29 12:55:16 +0200

    tekst i komponenty do experience (pc)

---
+++ src/components/contents/experience.tsx
@@ -0,0 +1,105 @@
+import AText from "components/animations/animatedText";
+import { LegacyRef, forwardRef } from "react";
+import 'styles/content.css';
+
+type DivRef = LegacyRef<HTMLDivElement>;
+
+const Section = forwardRef((props: {title: string, content: string[]}, ref: DivRef) => {
+    return (
+        <div className="experience-section" ref={ref}>
+            <div className="experience-section-header">
+                <AText>{props.title}</AText>
+            </div>
+            <div className="experience-section-text">
+                {props.content.map((text, index) => (
+                    <AText d={(index + 4) / 10}>{text}</AText>
+                ))}
+            </div>
+        </div>
+    );
+});
+
+const SectionOne = forwardRef((props, ref: DivRef) => {
+    return <Section
+        ref={ref} 
+        title="School app" 
+        content={[
+            "While me and my friends were making up school council, we had an idea to create some big mobile app for school community",
+            "And we have created it",
+            "App is posted on App Store and Apple Play, and once we had 200 active users across out high school only",
+            "Our team was created of class mates, helping and learning from each other",
+            "It is the biggest thing i have ever created",
+            "Built in React Native and Express JS - Stinder"
+        ]}
+    />;
+});
+
+const SectionTwo = forwardRef((props, ref: DivRef) => {
+    return <Section
+        ref={ref} 
+        title="Various projects for fun" 
+        content={[
+            "I have written a lot of different small projects, usually not finished them",
+            "I was having fun creating some games in C++ from Youtube videos",
+            "There was also some useful programs, or small experiences with developing Android app with Java",
+            "Generally when I don't have idea what to do today, I think what what could I continue or start new "
+        ]}
+    />;
+});
+
+
+const SectionThree = forwardRef((props, ref: DivRef) => {
+    return <Section
+        ref={ref} 
+        title="C++ Website" 
+        content={[
+            "Once as school project I had to create website",
+            "As someone who hadn't known anything, i chose to do it in standard HTML, PHP and js",
+            "In spite of lack of knowledge, it turned out to be good looking and well functioning",
+            "Website had user accounts system and couple of sub-sites describing basics of C++ language"
+        ]}
+    />;
+});
+
+const SectionFour = forwardRef((props, ref: DivRef) => {
+    return <Section
+        ref={ref} 
+        title="Theoretical Informatics" 
+        content={[
+            "Since 2020 i have stayed in constant touch with algorithmic programming",
+            "I have started multiple times in Polish Olympiad of Informatics",
+            "While preparing, I was taught numerous algorithms and ideas and also learned kind of algorithmic thinking",
+            "I liked it, i was not a master, but i would consider myself as alright in this field",
+            "Creating something, that some one can use now gives me much more satisfaction"
+        ]}
+    />;
+});
+
+const SectionFive = forwardRef((props, ref: DivRef) => {
+    return <Section
+        ref={ref} 
+        title="Advanced Projects" 
+        content={[
+            "After gathering experience from small fun projects, i moved on to creating bigger services",
+            "I was working on things like product warehouse management app, hosting events website or kind of market app",
+            "Taking care of more serious things, taught me a lot and also got me plenty of experience"
+        ]}
+    />;
+});
+
+
+
+const ExperienceContent = {
+    SectionOne,
+    SectionTwo,
+    SectionThree,
+    SectionFour,
+    SectionFive,
+    SectionOneImg: "https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg",
+    SectionTwoImg: "https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg",
+    SectionThreeImg: "https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg",
+    SectionFourImg: "https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg",
+    SectionFiveImg: "https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg",
+};
+
+export default ExperienceContent;
--- src/screens/experience.tsx
+++ src/screens/experience.tsx
@@ -1,5 +1,6 @@
 import AnimatedBox from "components/animations/animatedBox";
 import BackToHome from "components/backToHome";
+import ExperienceContent from "components/contents/experience";
 import AchievedSVG from "components/handwrites/achieved";
 import ExperienceSVG from "components/handwrites/experience";
 import useScrollPosition from "hooks/scrollPosition";
@@ -29,29 +30,29 @@ const Experience = () => {
     const textPartRef = useRef<HTMLDivElement>(null);
     const { scrollY } = useScrollPosition(textPartRef);
     const { size: {width, height} } = useWindowSize();
+
     const sectionOneRef = useRef<HTMLDivElement>(null);
     const sectionTwoRef = useRef<HTMLDivElement>(null);
     const sectionThreeRef = useRef<HTMLDivElement>(null);
-    const [startPosition, setStartPosition] = useState(0);
+    const sectionFourRef = useRef<HTMLDivElement>(null);
+    const sectionFiveRef = useRef<HTMLDivElement>(null);
+
     const [imageOneY, setImageOneY] = useState(0);
     const [imageTwoY, setImageTwoY] = useState(0);
     const [imageThreeY, setImageThreeY] = useState(0);
+    const [imageFourY, setImageFourY] = useState(0);
+    const [imageFiveY, setImageFiveY] = useState(0);
+
+    const [startPosition, setStartPosition] = useState(0);
 
     const handleTextScroll = (e: any)=> {
-        const scrollPosition = textPartRef.current?.scrollTop || 0;
-        //cannot use scrollY, don't know why
-
-        const arr = [sectionOneRef, sectionTwoRef, sectionThreeRef];
+        const arr = [sectionOneRef, sectionTwoRef, sectionThreeRef, sectionFourRef, sectionFiveRef];
 
         let minDist = Infinity;
         let minIndex = 0;
-        for(let i=0; i<=2; i++){
+        for(let i=0; i<=4; i++){
             const sizes = arr[i].current!.getBoundingClientRect();
             console.log(i, ' -> ', sizes.top);
-
-            // if(sizes.top - startPosition < 0 && sizes.bottom - startPosition > textPartRef.current!.clientHeight/2){
-            //     return;
-            // }
             
             if(Math.abs(sizes.top - startPosition - 150) <= minDist){
                 minDist = Math.abs(sizes.top - startPosition - 150);
@@ -75,6 +76,14 @@ const Experience = () => {
 
         setImageThreeY(
             (sectionThreeRef.current!.getBoundingClientRect().top - startPosition) * 3
+        );
+
+        setImageFourY(
+            (sectionFourRef.current!.getBoundingClientRect().top - startPosition) * 3
+        );
+
+        setImageFiveY(
+            (sectionFiveRef.current!.getBoundingClientRect().top - startPosition) * 3
         );
 
     }, [scrollY, startPosition]);
@@ -101,14 +110,19 @@ const Experience = () => {
             <AnimatedBox className="h-screen w-screen p-20 mobile-flex split-2">
                 <div className="half-container h-full overflow-y-hidden">
                     <SectionImage translateY={imageOneY} 
-                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg"/>
+                        src={ExperienceContent.SectionOneImg}/>
 
                     <SectionImage translateY={imageTwoY} 
-                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo.jpg"/>
-
+                        src={ExperienceContent.SectionOneImg}/>
 
                     <SectionImage translateY={imageThreeY} 
-                        src="https://www.staszic.waw.pl/media/gallery/szkola/photo_5.jpg"/>
+                        src={ExperienceContent.SectionOneImg}/>
+
+                    <SectionImage translateY={imageFourY} 
+                        src={ExperienceContent.SectionOneImg}/>
+
+                    <SectionImage translateY={imageFiveY} 
+                        src={ExperienceContent.SectionOneImg}/>
                 </div>
                 <div
                     className="half-container text-part overflow-y-hidden"
@@ -118,98 +132,11 @@ const Experience = () => {
                     </div>
                     <div className="achieved-text-container h-full">
                         <div className="achieved-text font-1 overflow-y-scroll" ref={textPartRef}>
-                            <div className="experience-section">
-                                <div className="experience-section-header" ref={sectionOneRef}>
-                                    General what have i been doing - 1111
-                                </div>
-                                <div className="experience-section-text">
-                                    Lorem, ipsum dolor sit amet consectetur
-                                    adipisicing elit. Impedit eligendi
-                                    voluptatum quasi sunt. Commodi, ab odio
-                                    nobis hic mollitia perspiciatis culpa labore
-                                    expedita ducimus autem cupiditate alias
-                                    adipisci sapiente, asperiores nemo magni,
-                                    molestiae sit similique! Impedit sunt ipsam
-                                    quibusdam porro animi unde, quisquam vel
-                                    autem non amet consequatur! Unde adipisci
-                                    consequatur vel, eius aliquid dignissimos
-                                    hic veniam fuga. Debitis ab sapiente harum
-                                    rerum molestiae distinctio ea odit officiis
-                                    maxime vitae consequatur, ducimus voluptates
-                                    asperiores temporibus, provident, velit
-                                    adipisci. Placeat totam iure facere
-                                    corrupti, ratione accusantium nulla
-                                    exercitationem tenetur dicta impedit
-                                    cupiditate, harum fugit id, inventore quo ad
-                                    sit? Iste, unde.
-                                    quibusdam porro animi unde, quisquam vel
-                                    autem non amet consequatur! Unde adipisci
-                                    consequatur vel, eius aliquid dignissimos
-                                    hic veniam fuga. Debitis ab sapiente harum
-                                    rerum molestiae distinctio ea odit officiis
-                                    maxime vitae consequatur, ducimus voluptates
-                                    asperiores temporibus, provident, velit
-                                    adipisci. Placeat totam iure facere
-                                    corrupti, ratione accusantium nulla
-                                    exercitationem tenetur dicta impedit
-                                    cupiditate, harum fugit id, inventore quo ad
-                                    sit? Iste, unde.
-                                </div>
-                            </div>
-
-                            <div className="experience-section" ref={sectionTwoRef}>
-                                <div className="experience-section-header">
-                                    General what have i been doing - 222
-                                </div>
-                                <div className="experience-section-text">
-                                    Lorem, ipsum dolor sit amet consectetur
-                                    adipisicing elit. Impedit eligendi
-                                    voluptatum quasi sunt. Commodi, ab odio
-                                    nobis hic mollitia perspiciatis culpa labore
-                                    expedita ducimus autem cupiditate alias
-                                    adipisci sapiente, asperiores nemo magni,
-                                    molestiae sit similique! Impedit sunt ipsam
-                                    quibusdam porro animi unde, quisquam vel
-                                    autem non amet consequatur! Unde adipisci
-                                    consequatur vel, eius aliquid dignissimos
-                                    hic veniam fuga. Debitis ab sapiente harum
-                                    rerum molestiae distinctio ea odit officiis
-                                    maxime vitae consequatur, ducimus voluptates
-                                    asperiores temporibus, provident, velit
-                                    adipisci. Placeat totam iure facere
-                                    corrupti, ratione accusantium nulla
-                                    exercitationem tenetur dicta impedit
-                                    cupiditate, harum fugit id, inventore quo ad
-                                    sit? Iste, unde.
-                                </div>
-                            </div>
-
-                            <div className="experience-section" ref={sectionThreeRef}>
-                                <div className="experience-section-header">
-                                    General what have i been doing - 3333
-                                </div>
-                                <div className="experience-section-text">
-                                    Lorem, ipsum dolor sit amet consectetur
-                                    adipisicing elit. Impedit eligendi
-                                    voluptatum quasi sunt. Commodi, ab odio
-                                    nobis hic mollitia perspiciatis culpa labore
-                                    expedita ducimus autem cupiditate alias
-                                    adipisci sapiente, asperiores nemo magni,
-                                    molestiae sit similique! Impedit sunt ipsam
-                                    quibusdam porro animi unde, quisquam vel
-                                    autem non amet consequatur! Unde adipisci
-                                    consequatur vel, eius aliquid dignissimos
-                                    hic veniam fuga. Debitis ab sapiente harum
-                                    rerum molestiae distinctio ea odit officiis
-                                    maxime vitae consequatur, ducimus voluptates
-                                    asperiores temporibus, provident, velit
-                                    adipisci. Placeat totam iure facere
-                                    corrupti, ratione accusantium nulla
-                                    exercitationem tenetur dicta impedit
-                                    cupiditate, harum fugit id, inventore quo ad
-                                    sit? Iste, unde.
-                                </div>
-                            </div>
+                            <ExperienceContent.SectionOne ref={sectionOneRef}/>
+                            <ExperienceContent.SectionTwo ref={sectionTwoRef}/>
+                            <ExperienceContent.SectionThree ref={sectionThreeRef}/>
+                            <ExperienceContent.SectionFour ref={sectionFourRef}/>
+                            <ExperienceContent.SectionFive ref={sectionFiveRef}/>
                         </div>
                     </div>
                 </div>
