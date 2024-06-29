0247ec41c852a18f5923840b437d577d3957bbab
Jan Ancuta <jas.ancuta@gmail.com>
2024-06-29 13:06:58 +0200

    tekst do today

---
+++ src/components/contents/today.tsx
@@ -0,0 +1,14 @@
+import AText from "components/animations/animatedText";
+
+const TodayContent = () => {
+    return (
+        <div className="text-content today">
+            <AText d={0.3}>This days, after finishing high school I wait for new semester on university</AText>
+            <AText d={0.4}>So I wanted to learn something new and gain new experience</AText>
+            <AText d={0.7}>That is why I would like to work in already developed, bigger company</AText>
+            <AText d={0.8}>I thing that I would perform the best at things that i am already familiar with, so generally developing software</AText>
+        </div>
+    )
+};
+
+export default TodayContent;
